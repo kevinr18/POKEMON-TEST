e6f1d24c47407d8550c2a158532335895eb365c9
desarrollador7 <desarrollador7@softcapla.com>
2020-06-11 12:04:08 -0400

    Entorno Virutal (pokemonEnv)

---
+++ pokemonEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\halgu\OneDrive\Escritorio\Proyectos\Proyecto_turpial\proyecto_pokemon\pokemonEnv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
